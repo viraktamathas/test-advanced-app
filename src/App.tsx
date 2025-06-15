501e8b699a414fa3e3d0f682469b4e0f120c1819
Amit Viraktamath <amit.viraktamath@nih.gov>
2025-06-15 18:48:16 -0400

    Add src/App.tsx from react-app template

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>test-advanced-app</h1>
+        <p>for testing the new advanced workflow implementation</p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
